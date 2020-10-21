1df5295a806c2318bba45ec23babd55f6388eeac
Camilo Galeano <juan_camilo50@hotmail.com>
2020-10-21 23:31:45 +0200

    created main and object-field components

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+
+// Component which holds all of the consume-object fields
+
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
---
+++ src/app/components/object-field/object-field.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-object-field',
+  templateUrl: './object-field.component.html',
+  styleUrls: ['./object-field.component.css']
+})
+export class ObjectFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
